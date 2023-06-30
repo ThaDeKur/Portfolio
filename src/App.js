dc3ee81d61be33b624f88ea3d2eac84b8a008da0
ThaDeKur <deepakkumarthakur8010@gmail.com>
2023-06-30 13:02:05 +0530

    Jumbotron template added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import PageHome from './Home/PageHome';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      {PageHome()}
     </div>
   );
 }
